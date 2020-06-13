a29f4e12a52fc64b4eabc2b003cc7f682e11df65
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-06-13 18:53:46 +0300

    Scriplet. Динамическая таблица.[276520#297286]

---
+++ src/main/java/ru/job4j/dream/model/Post.java
@@ -0,0 +1,48 @@
+package ru.job4j.dream.model;
+
+import java.util.Objects;
+
+/**
+ * Модель - вакансия
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 13.06.2020
+ * @version 1.0
+ */
+public class Post {
+    private int id;
+    private String name;
+
+    public Post(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -0,0 +1,33 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Post;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Хранилище вакансий (синглтон)
+ * @author Roman Yakimkin
+ * @since 13.06.2020
+ * @version 1.0
+ */
+public class Store {
+    private static final Store INST = new Store();
+
+    private Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private Store() {
+        posts.put(1, new Post(1, "Junior Java Job"));
+        posts.put(2, new Post(2, "Middle Java Job"));
+        posts.put(3, new Post(3, "Senior Java Job"));
+    }
+
+    public static Store instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
