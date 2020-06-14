eb0ecdbc3a9917ee5876a6a76096cbef4f8bdfe2
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-06-14 14:11:03 +0300

    candicates.jsp - список кандидатов.[276522#297288]

---
+++ src/main/java/ru/job4j/dream/model/Candidate.java
@@ -0,0 +1,49 @@
+package ru.job4j.dream.model;
+
+import java.util.Objects;
+
+/**
+ * Класс - кандидат на должность
+ * @author Roman Yakimkin
+ * @since 14.06.2020
+ * @version 1.0
+ */
+public class Candidate {
+    private int id;
+    private String name;
+
+    public Candidate(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Candidate candidate = (Candidate) o;
+        return id == candidate.id &&
+                Objects.equals(name, candidate.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
