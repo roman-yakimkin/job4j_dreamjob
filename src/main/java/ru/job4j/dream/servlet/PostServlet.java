86f3892fbc1c42cfb4e2401f86eb0f72ee8330c5
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-06-15 14:04:24 +0300

    Servlet. Web.xml[6866#297298]

---
+++ src/main/java/ru/job4j/dream/servlet/PostServlet.java
@@ -0,0 +1,25 @@
+package ru.job4j.dream.servlet;
+
+import ru.job4j.dream.model.Post;
+import ru.job4j.dream.store.Store;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Сервлет для сохранения вакансии
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @version 1.0
+ * @since 15.06.2020
+ */
+public class PostServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        req.setCharacterEncoding("UTF-8");
+        Store.instOf().save(new Post(0, req.getParameter("name")));
+        resp.sendRedirect(req.getContextPath() + "/posts.jsp");
+    }
+}
--- src/main/webapp/post/edit.jsp
+++ src/main/webapp/post/edit.jsp
@@ -28,10 +28,10 @@
                 Новая вакансия.
             </div>
             <div class="card-body">
-                <form>
+                <form action="<%=request.getContextPath()%>/post/save" method="post">
                     <div class="form-group">
                         <label>Имя</label>
-                        <input type="text" class="form-control">
+                        <input type="text" name="name" class="form-control">
                     </div>
                     <button type="submit" class="btn btn-primary">Сохранить</button>
                 </form>
