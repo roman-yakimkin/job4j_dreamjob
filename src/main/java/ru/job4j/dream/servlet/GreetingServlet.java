bd5c2b63e7a0411243c40932b1ed34deac36aaa1
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-06-21 19:37:43 +0300

    4.1. JSON формат.[238554#297274]

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,11 +1,18 @@
 package ru.job4j.dream.servlet;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Map;
 
 /**
  * Сервлет для отправки приветствия
@@ -22,5 +29,20 @@ public class GreetingServlet extends HttpServlet {
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
         writer.println("Nice to meet you, " + name);
         writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        JSONParser jsonParser = new JSONParser();
+        try {
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(req.getReader());
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            String name = jsonObject.get("name").toString();
+            String jsonOutput = JSONObject.toJSONString(Map.of("name", "Nice to meet you, " + name));
+            resp.getWriter().write(jsonOutput);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
